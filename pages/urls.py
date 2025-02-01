9f6289cdc90d7877da6d70c55e37592ecb8e0e7e
shawndutill@gmail.com <shawndutill@gmail.com>
2025-02-01 11:36:00 -0500

    Adding recipes page & path in base template

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, RecipePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("recipes/", RecipePageView.as_view(), name="recipes"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+class RecipePageView(TemplateView):
+    template_name = "recipes.html"
