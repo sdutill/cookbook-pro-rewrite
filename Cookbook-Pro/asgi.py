236d085ecc1e339fcbca6e0617df32b9733f0f3e
shawndutill@gmail.com <shawndutill@gmail.com>
2025-02-01 13:49:42 -0500

    Renamed project to Cookbook-Pro

--- Cookbook-Pro/asgi.py
+++ Cookbook-Pro/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Cookbook project.
+ASGI config for Cookbook-Pro project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Cookbook.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Cookbook-Pro.settings")
 
 application = get_asgi_application()
