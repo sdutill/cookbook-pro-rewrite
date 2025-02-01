beb9de0acff36901a4562c71d57d67fde9b01362
shawndutill@gmail.com <shawndutill@gmail.com>
2025-02-01 12:57:45 -0500

    Added public & private directories & updated paths to templates

--- pages/tests.py
+++ pages/tests.py
@@ -10,5 +10,5 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_view(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(response, "public/home.html")
         self.assertContains(response, "Home")
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = "home.html"
+    template_name = "public/home.html"
 
 
-class RecipePageView(TemplateView):
-    template_name = "recipes.html"
+class RecipePageView(LoginRequiredMixin, TemplateView):
+    template_name = "private/recipes.html"
+    login_url = "login"
